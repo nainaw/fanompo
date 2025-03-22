9eca2b9a81742f0eabaadcdafa05622e5ae06064
wnaina <39332359+nainaw@users.noreply.github.com>
2025-03-22 20:16:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const spreadsheetId = '1FABCdEXAMPLE_URL_GOESHERE';
 const apiKey = 'YOUR_GOOGLE_API_KEY';
 
 // Construct the URL for Google Sheets API v4
-const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/Sheet1?key=${apiKey}`;
+const url = `https://sheets.googleapis.com/v4/spreadsheets/1O4gm7x7dR8dfzUbs8UnQ6o0ykzA6Oa_DZ6NEfQDfLKE/values/API?alt=json&key=AIzaSyCmERnqacZ3WTdnvTL5IrfWyjoM48U7STI`;
 
 async function fetchGoogleSheetData() {
     try {
